f6592f0ed9d558121cbfd41115d266bb793daac3
leon <109093697+leonlynn1001@users.noreply.github.com>
2023-04-29 17:42:14 +0630

    second method

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ questions.forEach((item) => {
   const btn = item.querySelector(".question-btn");
   console.log(btn);
   btn.addEventListener("click", (e) => {
-    const post =
-      e.target.parentElement.parentElement.parentElement.parentElement;
-
-    post.classList.toggle("show-text");
-   
+    questions.forEach((question) => {
+      console.log(question);
+      if (question !== item) {
+        question.classList.remove("show-text");
+      }
+    });
+    item.classList.toggle("show-text");
   });
+  //   btn.addEventListener("click", (e) => {
+  //     const post =
+  //       e.target.parentElement.parentElement.parentElement.parentElement;
+
+  //     post.classList.toggle("show-text");
+  //   });
 });
